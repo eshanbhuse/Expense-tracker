d6348244c1e6f86eb6983d1f1733e13aff2adf9a
Eshan Bhuse <eshan.bhuse@spit.ac.in>
2025-09-09 00:49:34 +0530

    updated apiPaths to backend url

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
 
-export const BASE_URL = 'https://expense-tracker-backend-beta-three.vercel.app';
+export const BASE_URL = 'https://expense-tracker-backend-project-rho.vercel.app/';
 export const API_PATHS = {
     AUTH:{
     LOGIN: '/api/v1/auth/login',
