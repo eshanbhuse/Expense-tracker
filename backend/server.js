86339dabda16f6f22c18e340fc493481523dd1a8
Eshan Bhuse <eshan.bhuse@spit.ac.in>
2025-09-10 01:52:44 +0530

    undo

--- backend/server.js
+++ backend/server.js
@@ -62,13 +62,5 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // app.listen(PORT,()=>
 //     console.log(`Server is running on port ${PORT}`));
 
-// Serve React frontend
-const frontendPath = path.join(__dirname, "/build"); // Make sure build folder exists
-app.use(express.static(frontendPath));
-
-// Catch-all route to handle React routing (fixes 404 on refresh)
-app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-});
 
 module.exports = app;
