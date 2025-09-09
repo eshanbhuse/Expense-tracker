601d28b2aff1ac9a032c3fa85c77f13666ac77ab
Eshan Bhuse <eshan.bhuse@spit.ac.in>
2025-09-09 22:24:55 +0530

    server.js code updated

--- backend/server.js
+++ backend/server.js
@@ -2,34 +2,33 @@
 require("dotenv").config()
 const express = require("express")
 const path = require("path")
-const cors = require("cors")
 const connectDB = require("./config/db")
 const authRoutes = require("./routes/authRoutes")
 const incomeRoutes = require("./routes/incomeRoutes")
 const expenseRoutes = require("./routes/expenseRoutes")
 const dashboardRoutes = require("./routes/dashboardRoutes")
 const app = express()
-// const cors = require('cors');
+const cors = require('cors');
 
-// const allowedOrigins = [
-//     'http://localhost:5173',          // Local development
-//     'https://expense-tracker-sandy-tau-96.vercel.app/login'             // Production frontend URL (set in .env or Vercel)
-// ];
+const allowedOrigins = [
+    'http://localhost:5173',          // Local development
+    'https://expense-tracker-sandy-tau-96.vercel.app/login'             // Production frontend URL (set in .env or Vercel)
+];
 
-// app.use(cors({
-//     origin: function(origin, callback) {
-//         // Allow requests with no origin (like Postman or server-to-server)
-//         if (!origin) return callback(null, true);
-//         if (allowedOrigins.indexOf(origin) !== -1) {
-//             callback(null, true); // Allowed
-//         } else {
-//             callback(new Error('Not allowed by CORS')); // Blocked
-//         }
-//     },
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true
-// }));
+app.use(cors({
+    origin: function(origin, callback) {
+        // Allow requests with no origin (like Postman or server-to-server)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true); // Allowed
+        } else {
+            callback(new Error('Not allowed by CORS')); // Blocked
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 
 // app.use(express.json());
 
